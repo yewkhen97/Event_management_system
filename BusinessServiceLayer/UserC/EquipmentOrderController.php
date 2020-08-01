7f1d70ca54936edc7adbe4356b739c2c7be2d0ab
yewkhenyong97 <69064335+yewkhenyong97@users.noreply.github.com>
2020-08-01 10:45:56 +0800

    Development-controller for supplier
    controller for equipment order, approve supplier and supplier status

--- BusinessServiceLayer/UserC/EquipmentOrderController.php
+++ BusinessServiceLayer/UserC/EquipmentOrderController.php
@@ -1,41 +1,62 @@
 <?php
-
-include '../../BusinessServiceLayer/UserM/sEquipmentOrderModel.php';
-if(isset($_GET["id"])){
-
-$id1=$_GET['id'];
-
-include '../UserM/sEquipmentOrderModel.php';
-
+function EOorderEquipment($cusID,$eqID,$eqQuantity,$eqPrice){
+    include '../UserM/EquipmentModel.php';
 }
 
+
+function EOdeleteEquipment(){
+
+    include '../UserM/EquipmentModel.php';
+}
+
+
+
+function displayOrder(){
+	include '../../BusinessServiceLayer/UserM/EquipmentModel.php';
+	$result2 = fetchOrder($_SESSION['id']);
+	if ($result2-> num_rows > 0){
+		while ($row = $result2 -> fetch_assoc()){
+			// print out in table form
+
+		    $str = "";
+		    $str = "<tr>";
+		    ?>
+		    <form action="../../BusinessServiceLayer/UserM/EquipmentModel.php?id=<?php echo"$_SESSION[id]";?>&event=EOdelete" method="POST">
+		    <?php
+		    $str .= "<td style='display:none;'><input name='orderID' value='" . $row['EOd_orderID'] . "'/>";
+		    $str .= "<td style='display:none;'><input name='cusID' value='" . $row['C_customerID'] . "'/>";
+		    $str .= "<td>" . $row['EOd_orderID'] . "</td>";
+		    $str .= "<td>" . $row['EQ_equipmentName'] . "</td>";
+		    $str .= "<td>RM " . $row['EOd_quantity'] . "</td>";
+		    $str .= "<td>" . $row['EOd_totalPrice'] . "</td>";
+		    $str .= "<td><input type='submit' value='Delete'></td>";
+		    // add other td here if there's more
+
+		    // end of tr
+		    $str .="</form>";
+		    $str .= "</tr>";
+		    echo $str;
+		}
+	}
+}
+
+?>
+
+
+<?php
 if(isset($_GET["event"])){
 $ev=$_GET['event'];
 
-$sup=$_SESSION["id"];
- if ($ev=='sc'){
 
- echo '<script type="text/javascript">'; 
-echo 'alert("connect successful");'; 
-echo 'window.location.href = "../../ApplicationLayer/UserView/supplierMenu.php";';
-echo '</script>';
-
-
+if($ev=='EOadd'){
+	$total=0;
+	$total=$_POST["quantity"]*$_POST["price"];
+	EOorderEquipment($_POST["cusID"],$_POST["eqID"],$_POST["quantity"],$total);
 }
-else if ($ev=='fail'){
-echo '<script type="text/javascript">'; 
-echo 'alert("connect fail, please contact admin");'; 
-echo 'window.location.href = "../../ApplicationLayer/UserView/supplierMenu.php";';
-echo '</script>';
-
-	
+else if ($ev=='E0delete'){
+	E0deleteEquipment();
 }
 
 
-
 }
-
-
-
-
 ?>
