03aeef1b72a3f1159fa4402fa1b4aeeb83cef04c
tzehonghong <69057088+tzehonghong@users.noreply.github.com>
2020-08-01 08:09:22 +0800

    Add files via upload

---
+++ ApplicationLayer/UserView/cPaymentCancel.php
@@ -0,0 +1,52 @@
+
+<?php
+session_start();
+//if(isset($_SESSION["id"])){
+  //  session_destroy();
+//}
+
+
+
+  
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+  <?php
+   include 'customerHeader.php';
+?>
+            <html>
+<head>
+<title>Order Placed</title>
+<style>
+.response-text {
+    display: inline-block;
+    max-width: 550px;
+    margin: 0 auto;
+    font-size: 1.5em;
+    text-align: center;
+    background: #fff3de;
+    padding: 42px;
+    border-radius: 3px;
+    border: #f5e9d4 1px solid;
+    font-family: arial;
+    line-height: 34px;
+}
+</style>
+</head>
+<body>
+    <div class="response-text">
+      Sorry! Payment Cancelled.
+    </div>
+</body>
+</html>
+  </div>
+        <!-- /.container-fluid -->
+
+      <?php
+         include 'customerFooter.php';
+         ?>
+</html>
