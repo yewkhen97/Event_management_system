02b386641723f42f171aee80d79d2af14b47dea0
weinengt <57633377+weinengt@users.noreply.github.com>
2020-08-02 04:33:04 +0800

    Revert "Update index.php"

--- ApplicationLayer/UserView/index.php
+++ ApplicationLayer/UserView/index.php
@@ -371,9 +371,7 @@ span.psw {
           </ol>
 
           <!-- Page Content -->
-
-          <h1>Select Your Member Type UPDATED UPDATED BLA BLA</h1>
-
+          <h1>Select Your Member Type</h1>
           <hr>
              <!-- Icon Cards-->
           <div class="row">
