0135ff037dfbfc04764efec259248e3e7070b404
tzehonghong <69057088+tzehonghong@users.noreply.github.com>
2020-08-02 21:59:42 +0800

    Update cPaymentCheckout.php
    payment update

--- ApplicationLayer/UserView/cPaymentCheckout.php
+++ ApplicationLayer/UserView/cPaymentCheckout.php
@@ -70,6 +70,7 @@ $totalpayment = 0;
 <div id="payment-box">
 <?php
 		// Start for Cart list for Events -Darrell
+        echo "HELLO!!!!!!";
         echo "<h4>$events</h4>";
         echo "<table width=100%>
         <tr>
